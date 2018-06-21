8b9c4e2d8af6dc74389099531fd1e9654845349a
anzohra@outlook.com <anzohra@outlook.com>
2018-06-21 23:32:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map 
+  planeteer_calls.map { |call| call.capitalize
 end
 
 def long_planeteer_calls# code an argument here
