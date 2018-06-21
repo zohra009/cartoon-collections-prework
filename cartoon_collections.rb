01bd7d6568761c610ceac26666c81448d3007f6a
anzohra@outlook.com <anzohra@outlook.com>
2018-06-21 23:28:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,9 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each.with_index(1) do |dwarves, index|
+  dwarves.each_with_index(1) do |dwarves, index|
   puts "#{index}. #{dwarves}"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet()
 end
 
 def long_planeteer_calls# code an argument here
