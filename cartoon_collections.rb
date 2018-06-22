a36826f7b94b97f39db08470afa58257ebfcc149
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 00:31:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,10 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese(chedder_cheese)
-  cheeses = %w(gouda chedder camembert)
-  
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w(gouda cheddar camembert)
+
   potentially_cheesy_items.find do |maybe_cheese|
-    cheese.include?(maybe_cheese)
+    cheeses.include?(maybe_cheese)
   end
 end
